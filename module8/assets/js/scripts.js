44ff7cb2c33a4a3f99902fe861448e1b981b39f0
petra vincent <petra_vincent@mckinsey.com>
2016-03-07 14:11:15 -0500

    new setup

--- module8/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("ul.navbar-nav li a").click(function(event) {
-        $(this).parents(".navbar-collapse").removeClass('in');
-        console.log($(this).parents(".navbar-collapse").attr("aria-expanded", "false"));
-    });
-});
